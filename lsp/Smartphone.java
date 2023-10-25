59c334a565dd1197b49dfea67ba002cf40101b02
Ranti Maharani <rantimaharani1011@gmail.com>
2023-10-25 08:46:10 +0700

    perubahan sesuai prinsip liskov subtitution

--- lsp/Smartphone.java
+++ lsp/Smartphone.java
@@ -1,14 +1,8 @@
 package lsp;
 
-import java.util.Date;
-
 public class Smartphone extends Product{
     @Override
     String getName(){
         return "Samsung s22 Ultra";
-    }
-    @Override
-    Date getExpiredDate(){
-        return new Date();
     }
 }
